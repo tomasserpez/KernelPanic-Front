5d64dec7422a9ca68a333d7d0c7c2a46ec5666f1
Ezequiel Gonzalez L <39284635+Calefon@users.noreply.github.com>
2024-11-01 19:38:32 -0300

    arreglado logo registro y login

--- src/Login.tsx
+++ src/Login.tsx
@@ -29,8 +29,11 @@ export default function Login() {
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                        <img src="./assets/img/logoSpaceTraders.png" alt="logo" />
-                        <p className="font-Revalia text-purple-600 text-3xl text-center">SPACE TRADERS</p>
+                        <div className="flex flex-row items-center space-between">
+                            <img src="./src/assets/img/logoSpaceTraders.png" alt="logo" className="w-12 me-3"/>
+                            <p className="font-Revalia text-purple-600 text-3xl text-center">SPACE TRADERS</p>
+                        </div>
+                        
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-purple-600 font-Revalia text-center">
                             Welcome
                         </h1>
