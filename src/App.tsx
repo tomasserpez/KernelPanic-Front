86e4e0f2543e3d8ea7ab0eda2146fb45ee28100f
Kevin Maximiliano Palma Romero <kev.mpr03@gmail.com>
2024-11-01 19:15:33 -0300

    Centrar el titulo space traders

--- src/App.tsx
+++ src/App.tsx
@@ -16,13 +16,13 @@ function App() {
       <div className="bg-gray-50 dark:bg-gray-900 h-screen flex flex-col items-start">
         <div className="flex flex-col w-3/5 rounded overflow-hidden shadow-lg m-16 ms-40 p-4">
           <div className="px-6 py-4">
-          <div className="font-bold text-7xl mb-2 text-slate-50 text-opacity-85">Bienvenido al cliente de SpaceTraders!</div>
+          <div className="font-bold text-7xl mb-2  text-opacity-85 font-Revalia text-purple-700">Bienvenido al cliente de SpaceTraders!</div>
         </div>
         <div className="px-6 pt-4 pb-2 flex flex-col m-11">
         {getUser() == null ? 
           <Link to={"/login"} className='text-xl font-medium text-primary-600 hover:underline dark:text-gray-200'>Ir al login</Link> 
           : 
-          <button onClick={cerrarSesionHandler} className="w-1/6 self-center text-white bg-primary-600 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-700 font-medium rounded-lg text-base px-5 py-2.5 text-center">Cerrar Sesión</button>
+          <button onClick={cerrarSesionHandler} className="text-lime-700 w-1/6 self-center bg-primary-600 hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-700 font-medium rounded-lg text-base px-5 py-2.5 text-center">Cerrar Sesión</button>
         }  
         </div>
       </div>
--- src/Login.tsx
+++ src/Login.tsx
@@ -30,7 +30,7 @@ export default function Login() {
                 <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                         <img src="./assets/img/logoSpaceTraders.png" alt="logo" />
-                        <p className="font-Revalia text-purple-600">SPACE TRADERS</p>
+                        <p className="font-Revalia text-purple-600 text-3xl text-center">SPACE TRADERS</p>
                         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-purple-600 font-Revalia text-center">
                             Welcome
                         </h1>
